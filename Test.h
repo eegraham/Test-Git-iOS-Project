30ed00d255afb1f8c751321dc865d42f9e35e627
Ellison Graham <ellison@Ellisons-MacBook-Air.local>
2014-10-06 16:52:24 -0400

    added test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Ellison Graham on 10/6/14.
+//  Copyright (c) 2014 Ellison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Ellison Graham on 10/6/14.
+//  Copyright (c) 2014 Ellison. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
